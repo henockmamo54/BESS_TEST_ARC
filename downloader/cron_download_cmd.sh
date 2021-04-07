1842006ad08b79879b9c9948bb41bf446ba0dfff
DESKTOP-9KF3V27\Henock <henockmamo54@gmail.com>
2021-04-07 14:36:16 +0900

    crontab schedule every saturday

--- downloader/cron_download_cmd.sh
+++ downloader/cron_download_cmd.sh
@@ -1,2 +1,3 @@
 cd ~/CronDownload 
-python ~/CronDownload/cron_download_script_log.py 
+python ~/CronDownload/cron_download_script.py  
+python ~/CronDownload/cron_download_script_log.py
---
+++ downloader/cron_download_script_root.sh
@@ -0,0 +1,4 @@
+cd ~/../home/Henock/CronDownload/
+python  ~/../home/Henock/CronDownload/cron_download_script.py 
+python  ~/../home/Henock/CronDownload/cron_download_script_log.py 
+ 
