09603c55f3e1050af65ac7a4f0705e521a0cb293
DESKTOP-9KF3V27\Henock <henockmamo54@gmail.com>
2021-06-06 13:51:16 +0900

    yearly download script added

---
+++ DownloaderYearly/Original/cron_download_cmd.sh
@@ -0,0 +1,3 @@
+cd /bess19/Henock/CronDownload
+python /bess19/Henock/CronDownload/cron_download_script.py  
+python /bess19/Henock/CronDownload/cron_download_script_log.py
---
+++ DownloaderYearly/Original/cron_download_script_root.sh
@@ -0,0 +1,4 @@
+cd ~/../bess19/Henock/CronDownload/ 
+python  ~/../bess19/Henock/CronDownload/cron_download_script.py
+python  ~/../bess19/Henock/CronDownload/cron_download_script_log.py 
+ 
