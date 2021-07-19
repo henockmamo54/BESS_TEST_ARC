b2544856cf7f5742b15cc01aeb89b7b9235b114f
DESKTOP-9KF3V27\Henock <henockmamo54@gmail.com>
2021-07-19 13:47:47 +0900

    download_MCD12C1 added

--- Download raw data.py
+++ Download raw data.py
@@ -7,10 +7,12 @@ Created on Mon Jul 19 10:51:35 2021
 
 
 import os
+import sys
+import numpy as np
 import modapsclient
+import urllib as urllib2
 from datetime import datetime
 from datetime import timedelta 
-import sys
 
 username = "hiik324"
 password = "Ecology123"
@@ -26,42 +28,83 @@ east="128.27222505323994"
 collection="61"
  
 startdate_obj = datetime.strptime(startdate, '%Y-%m-%d')
-enddate_obj = datetime.strptime(enddate, '%Y-%m-%d')
- 
+enddate_obj = datetime.strptime(enddate, '%Y-%m-%d') 
 path = "D:/Workplace/githubProjects/BESS_TEST_ARC/Data"
 
-products=["MCD12C1","MCD15A3H","MCD43D59","MCD43D60","MCD43D61","MCD43D62",
-          "MCD43D63","MOD15A2H","MOD44B"]
+
+def download_modis_products():
+    products=["MCD12C1","MCD15A3H","MCD43D59","MCD43D60","MCD43D61","MCD43D62",
+              "MCD43D63","MOD15A2H","MOD44B"]
+    
+    
+    for product in products:
+        print("Downloading -> ", product)
+        try:
+            for day in range (int((enddate_obj - startdate_obj).days)):
+                temp= startdate_obj + timedelta(days=day)  
+                
+                doy = int(datetime(temp.year,temp.month,temp.day).timetuple().tm_yday) 
+                
+                if(product=='MOD44B'):
+                    path_= os.path.join( path,product, str(temp.year))
+                else:
+                    path_= os.path.join( path,product, str(temp.year),str(doy))
+            
+                if not os.path.exists(path_):
+                    print(path_)
+                    os.makedirs(path_)
+                 
+                a = modapsclient.ModapsClient()
+                
+                
+                items=a.searchForFiles(products=product, startTime=temp, 
+                                          endTime=temp, north=north,south=south,
+                                          west=west,east=east, collection=collection)
+                print("Products count = > ",len(items))
+            
+            
+                # for p in items:
+                #     url=a.getFileUrls(p)[0]
+                #     print(p,url) 
+                #     # cmd=('wget  --user hiik324 --password Ecology123 {0} --header "Authorization: Bearer C88B2F44-881A-11E9-B4DB-D7883D88392C" -P {1} '.format( url, path_))
+                #     cmd=('wget  --user hiik324 --password Ecology123 {0}  -P {1} '.format( url, path_))
+                #     os.system(cmd)
+        except:
+            print("Error")
+            print("Unexpected error:", sys.exc_info()[0])
+            
+def download_MCD12C1():
+        
+    ROOT = 'https://e4ftl01.cr.usgs.gov/MOTA/MCD12C1.006' #MODIS/Terra+Aqua Land Cover Type CMG Yearly L3 Global 0.05 Deg
+     
+    years= np.unique(np.array([startdate_obj.year,enddate_obj.year]))
+    
+    for year in years:    
+       
+        #year = int(year)
+        USER = username
+        PASSWORD = password
+        YEAR = '%d' % (year)
+        path_ = os.path.join(path,YEAR)
+        if not os.path.exists(path_): os.makedirs(path_)
+        print ('Downloading MCD12C1, ' + YEAR)
+        PATH = '%s/%d.%02d.%02d' % (ROOT,year,1,1)
+        try: 
+            response = urllib2.urlopen(PATH)
+            for line in response.readlines():
+                ind = line.find('.hdf">')
+                if ind > 0:
+                    name = line[ind-34:ind+4]
+                    URL = '%s/%s' % (PATH,name)
+                    url = '%s/%s' % (path_,name)
+                    print (URL)
+                    os.system('wget --user %s --password %s -q -nc -O %s %s ' % (USER, PASSWORD, url,URL))
+        except:
+                pass
+     
 
 
-for product in products:
-    print("Downloading -> ", product)
-    try:
-        for day in range (int((enddate_obj - startdate_obj).days)):
-            temp= startdate_obj + timedelta(days=day)  
             
-            doy = int(datetime(temp.year,temp.month,temp.day).timetuple().tm_yday) 
-            
-            path_= os.path.join( path,product, str(temp.year),str(doy))
-        
-            if not os.path.exists(path_):
-                print(path_)
-                os.makedirs(path_)
-             
-            a = modapsclient.ModapsClient()
-            
-            
-            items=a.searchForFiles(products=product, startTime=startdate, 
-                                      endTime=enddate, north=north,south=south,
-                                      west=west,east=east, collection=collection)
-            print("Products count = > ",len(items))
-        
-        
-            # for p in items:
-            #     url=a.getFileUrls(p)[0]
-            #     print(p,url) 
-            #     cmd=('wget  --user hiik324 --password Ecology123 {0} --header "Authorization: Bearer C88B2F44-881A-11E9-B4DB-D7883D88392C" -P {1} '.format( url, path_))
-            #     os.system(cmd)
-    except:
-        print("Error")
-        print("Unexpected error:", sys.exc_info()[0])
+if __name__ == "__main__":
+    # download_modis_products()
+    download_MCD12C1()
